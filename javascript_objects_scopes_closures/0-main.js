e35f6d6a3d3bba74a8c6dbb85b4048c27cc7dd3c
YourGitHubUsername <youremail@example.com>
2023-09-11 04:34:30 -0700

    commit

--- javascript_objects_scopes_closures/0-main.js
+++ javascript_objects_scopes_closures/0-main.js
@@ -3,4 +3,5 @@ const Rectangle = require('./0-rectangle');
 
 const r1 = new Rectangle();
 console.log(r1);
-console.log(r1.constructor);
+console.log(r1.constructor.toString().split(' ')[1]);
+
--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,5 @@
 class Rectangle {
-  constructor() {}
 }
+
 module.exports = Rectangle;
+
