8408463dfd9e98131ca59317f5393da258613ff7
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-12 01:34:54 +0100

    commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,21 @@
 class Rectangle {
-    constructor(w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      } else {
-        Object.defineProperty(this, 'width', { value: undefined, writable: true });
-        Object.defineProperty(this, 'height', { value: undefined, writable: true });
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.defineProperty(this, 'width', { value: undefined, writable: true });
+      Object.defineProperty(this, 'height', { value: undefined, writable: true });
+    }
+  }
+
+  print() {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
       }
     }
   }
-  
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
