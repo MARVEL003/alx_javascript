e6b2723491b7330651140e9072bc0cfb499d25bc
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-12 01:02:04 +0100

    commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,6 @@ class Rectangle {
         this.width = w;
         this.height = h;
       } else {
-        // If w or h is not a positive integer or is equal to 0, create an empty object
-        // You can also set width and height to undefined or any other default value as needed
         Object.defineProperty(this, 'width', { value: undefined, writable: true });
         Object.defineProperty(this, 'height', { value: undefined, writable: true });
       }
