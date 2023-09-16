7707b4d237dbadbd8074fa32cad7c62712ded41f
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-16 20:30:47 +0100

    COMMIT

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,34 +1,28 @@
-const axios = require('axios');
+#!/usr/bin/node
 
-// URL to the Star Wars API films endpoint
-const apiUrl = process.argv[2]; // The API URL is passed as a command-line argument
+const req = require("request"); // Import the 'request' module.
+// Get the movie ID
+const url = `${process.argv[2]}`; //Star Wars API endpoint
 
-// Character ID for "Wedge Antilles"
-const characterId = 18;
-
-// Function to fetch the movies and count appearances of "Wedge Antilles"
-async function countMoviesWithCharacter(apiUrl, characterId) {
-  try {
-    const response = await axios.get(apiUrl);
-    const films = response.data.results;
-    let count = 0;
-
-    films.forEach((film) => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(count);
-  } catch (error) {
-    console.error('Error:', error.message);
+// Make a GET request to the API
+req(url, (error, response, body) => {
+  if (error) {
+    return console.error(error);
   }
-}
 
-// Check if the API URL is provided as a command-line argument
-if (!apiUrl) {
-  console.error('Please provide the API URL as the first argument.');
-} else {
-  // Call the function to count movies with "Wedge Antilles"
-  countMoviesWithCharacter(apiUrl, characterId);
-}
+  const data = JSON.parse(body);
+  const films = data.results;
+  let count = 0;
+  const characterId = "18"; // Character ID for Wedge Antilles
+
+  for (const film in films) {
+    const filmchars = films[film].characters;
+    for (const charIndex in filmchars) {
+      if (filmchars[charIndex].includes(characterId)) {
+        count += 1;
+      }
+    }
+  }
+
+  console.log(count);
+});
