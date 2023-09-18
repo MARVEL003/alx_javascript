babc9b34834f52e31d2f8852851044f0e0b72a1e
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-18 11:25:22 +0100

    commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,22 +1,18 @@
-// Import the request and fs modules
-const request = require("request");
 const fs = require("fs");
+const request = require("request");
 
-// Get the URL and the file path from the arguments
 const url = process.argv[2];
 const file_path = process.argv[3];
 
-// Use the request function to get the contents of the webpage
-request(url, (error, response, body) => {
-  // Check for errors
+request(url, function (error, response, body)
+{
+  
   if (error) {
     console.error(error);
     return;
   }
 
-  // Write the body response to the file using UTF-8 encoding
-  fs.writeFile(file_path, body, "utf8", (error) => {
-    // Check for errors
+  fs.writeFile(file_path, body, "utf8", function(error) {
     if (error) {
       console.error(error);
       return;
