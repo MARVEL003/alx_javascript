7214ac878ac1a0e986c60275dc6b3cfacc329a52
MARVELLOUS <chimchetarammarvel@gmail.com>
2023-09-22 20:16:41 +0100

    Closure commit

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+// create a function that will be used to send a message in the browser using alert
+function welcomeMessage(fullName) {
+    alert("welcome " + fullName);
+  }
+  
+  let guillaume = welcomeMessage("guillaume");
+  let alex = welcomeMessage("alex");
+  let fred = welcomeMessage("fred");
